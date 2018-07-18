7ca5d2f9811fa9672c2ac97d21594014d7b19493
Tony Hazama <tonyhazama4721@gmail.com>
2018-07-18 10:18:31 +0700

    add answer system

--- ToDo.java
+++ ToDo.java
@@ -1,12 +1,15 @@
 import java.util.Scanner;
 public class ToDo{
 	public static void main(String[] args){
-		String todothing = "";
+		String todothing, answer;
 		Scanner keypress = new Scanner(System.in);
 
 		System.out.print("What you want to do ? ");
 		todothing = keypress.nextLine();
 		System.out.println("So, you want to "+todothing+" huh ?");
 
+		System.out.print("Do you want to go with me ? ");
+		answer = keypress.nextLine();
+		System.out.println(answer+" ?");
 	}
 }
